808ab7c9f5c908a10075a63901d89cc420c5e1fd
Mina Ayman <mtrmena176@gmail.com>
2025-03-28 16:17:14 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kYellow = Color(0xFFFF9F0A);
+const kGrey = Color(0xFF5C5B60);
+const kBlack = Color(0xFF2A2A2C);
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IosCaclulator());
+}
+
+class IosCaclulator extends StatelessWidget {
+  const IosCaclulator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
